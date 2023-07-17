e8e668f88fb3d04734e8b525c2105026e600ed7c
David Bajollari <db@Davids-MBP.fios-router.home>
2023-07-17 16:52:43 -0400

    Add data sets

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import torch
 import numpy as np 
+import struct
+import matplotlib.pyplot as plt
 
-a = torch.FloatTensor(3, 2) 
 
-print(a)
+def main():
+    with open('train/train-images.idx3-ubyte','rb') as f:
+        data = f.read(8)
+        print(struct.unpack(">II", data))
+        # magic, size = struct.unpack(">II", f.read(8))
+        # nrows, ncols = struct.unpack(">II", f.read(8))
+        # data = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
+        # data = data.reshape((size, nrows, ncols))
+
+    
+    # plt.imshow(data[0,:,:], cmap='gray')
+    # plt.show()
+ 
+    X_train = 0  #training data
+    Y_train = 0  #training labels
+    X_test = 0  #test data
+    Y_test = 0  #test labels
+
+
+
+if __name__ == "__main__":
+    main()
